c505ec812ee810d064a4399925a8ca79cc358554
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 17:17:55 +0200

    task 5 new branch

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
