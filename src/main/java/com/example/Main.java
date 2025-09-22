e42ea3f654fd6ddb2e2ea7ce2460c38b5857ee59
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 17:07:10 +0200

    task 5 commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,8 +9,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the user name");
         String name = scanner.nextLine();
-        System.out.println("Welcome " + name);
-        
+        System.out.println("Welcome to the Ironhack course, " + name + "!");
+
         LocalDate date = LocalDate.now();
         System.out.println("Today is: " + date);
     }
