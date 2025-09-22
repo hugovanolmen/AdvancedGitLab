69ce7ca1e511acc3fbdf8dbc97afd3baa17a4a02
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 16:12:04 +0200

    task 2 commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the user name");
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name);
     }
 }
